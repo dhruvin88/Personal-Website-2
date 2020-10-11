47894e2bf9f2c01b6fddcf1344134fb7df71b8fc
Dhruvin <dhruvinp895@gmail.com>
2020-10-11 18:49:41 -0500

    Added AWS Amplify to project

--- profile-website/src/index.js
+++ profile-website/src/index.js
@@ -7,6 +7,10 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
